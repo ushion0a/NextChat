3c7e43de14ae6b2df286a787ef2514f3b5f8a6ac
ushion0a <66307812+ushion0a@users.noreply.github.com>
2026-02-04 01:58:32 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -566,7 +566,9 @@ const googleModels = [
   "gemini-2.0-pro-exp",
   "gemini-2.0-pro-exp-02-05",
   "gemini-2.5-pro-preview-06-05",
-  "gemini-2.5-pro"
+  "gemini-2.5-pro",
+  "gemini-3.0-pro-preview"
+  
 ];
 
 const anthropicModels = [
